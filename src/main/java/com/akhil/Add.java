e1323aff574985ee2de00aa36db020bc39dccde7
Akhil <gudise.akhil@gmail.com>
2021-06-01 19:41:21 +0530

    first commit

---
+++ src/main/java/com/akhil/Add.java
@@ -0,0 +1,16 @@
+package com.akhil;
+
+/**
+ * Hello world!
+ *
+ */
+public class Add 
+{
+    public static void main( String[] args )
+    {
+    	int x = Integer.parseInt(args[0]);
+    	int y = Integer.parseInt(args[1]);
+    	int ad = x + y;
+        System.out.println(ad);
+    }
+}
